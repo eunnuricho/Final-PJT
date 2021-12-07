9807b69ba5df021ee9f85b6ee480fdecd0a41773
조은누리 <jounuli@naver.com>
2021-12-07 15:26:00 +0900

    deleted secret key

--- final-pjt-back/finalpjt/settings.py
+++ final-pjt-back/finalpjt/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-nk48@e_)nzu-eve5a#eph_dq3*b6g9n-=h#*dl=mw=_u3065qn'
+SECRET_KEY = 'django-insecure-abcdef'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
